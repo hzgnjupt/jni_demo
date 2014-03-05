7f9888ed5e3bad3d01c28c702b7882aa7d26b11a
Administrator <Administrator@HZG-PC>
2014-03-05 20:25:53 +0800

    【jni_demo】初始提交

---
+++ jni_demo_java/src/hzgnjupt/demo/jni/JniDemo.java
@@ -0,0 +1,69 @@
+package hzgnjupt.demo.jni;
+
+import java.io.UnsupportedEncodingException;
+
+public class JniDemo {
+
+    private static JniDemo demo = new JniDemo();
+
+    static {
+        System.loadLibrary("jni_demo");
+    }
+
+    /**
+     * 参数为字节数组
+     * 
+     * @param data
+     * @param len
+     */
+    public native void argByteArray(byte[] data, int len);
+
+    private static void testArgByteArray() {
+        byte[] data = new String("jni_demo: argByteArray").getBytes();
+        demo.argByteArray(data, data.length);
+        System.out.println(new String(data));
+    }
+
+    /**
+     * 返回值为字节数组
+     * 
+     * @return
+     */
+    public native byte[] retByteArray();
+
+    private static void testRetByteArray() {
+        byte[] data = demo.retByteArray();
+        System.out.println(new String(data));
+    }
+
+    /**
+     * 参数为字符串
+     * 
+     * @param data
+     */
+    public native void argString(String data);
+
+    private static void testArgString() throws UnsupportedEncodingException {
+        demo.argString("jni_demo: argString");
+    }
+
+    /**
+     * 返回值为字符串
+     * 
+     * @return
+     */
+    public native String retString();
+
+    private static void testRetString() {
+        System.out.println(demo.retString());
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        testArgByteArray();
+        testRetByteArray();
+        testArgString();
+        testRetString();
+    }
+
+}
