b8c567cf11fd254ddbab20687603af18b97a179e
hzgnjupt <hzgnjupt@gmail.com>
2014-03-05 21:30:37 +0800

    init

---
+++ jni_demo_c/source/hzgnjupt_demo_jni_JniDemo.c
@@ -0,0 +1,42 @@
+#include "windows.h"
+#include "hzgnjupt_demo_jni_JniDemo.h"
+
+JNIEXPORT void JNICALL Java_hzgnjupt_demo_jni_JniDemo_argByteArray
+  (JNIEnv *env, jobject obj, jbyteArray data, jint len)
+{	
+	int i;
+	jbyte *b;
+	b = (*env)->GetByteArrayElements(env, data, JNI_FALSE);
+	for(i = 0; i < len; i++)
+	{
+		if(*(b + i) >= 'a' && *(b + i) <= 'z')
+		{
+			*(b + i) = *(b + i) + ('A' - 'a');
+		}
+	}
+	(*env)->SetByteArrayRegion(env, data, 0, len, b);
+}
+
+JNIEXPORT jbyteArray JNICALL Java_hzgnjupt_demo_jni_JniDemo_retByteArray
+  (JNIEnv *env, jobject obj)
+{
+	char *str = "jni_demo: retByteArray";
+	jbyteArray data = (*env)->NewByteArray(env, strlen(str));
+	(*env)->SetByteArrayRegion(env, data, 0, strlen(str), str);
+	return data;
+}
+
+JNIEXPORT void JNICALL Java_hzgnjupt_demo_jni_JniDemo_argString
+  (JNIEnv *env, jobject obj, jstring data)
+{
+	const char *str;
+	str = (*env)->GetStringUTFChars(env, data, JNI_FALSE);
+	printf("%s\n", str);
+	(*env)->ReleaseStringUTFChars(env, data, str);
+}
+
+JNIEXPORT jstring JNICALL Java_hzgnjupt_demo_jni_JniDemo_retString
+  (JNIEnv *env, jobject obj)
+{
+	return (*env)->NewStringUTF(env, "jni_demo: retString");
+}
