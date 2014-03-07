d25d32cc80191f7d0f96644f5400b8f7c80ef269
hzgnjupt <hzgnjupt@gmail.com>
2014-03-07 10:46:48 +0800

    修改create_h.bat，支持编译

--- jni_demo_java/create_h.bat
+++ jni_demo_java/create_h.bat
@@ -2,6 +2,8 @@ set JAVA_DIR=%~dp0
 set C_DIR=%JAVA_DIR%..\jni_demo_c
 set H_FILE=hzgnjupt_demo_jni_JniDemo.h
 
+javac -d %JAVA_DIR%bin -encoding utf-8 %JAVA_DIR%src\hzgnjupt\demo\jni\*.java
+
 del /f/s/q %C_DIR%\include\%H_FILE%
 
 cd /d %JAVA_DIR%bin
